03b1afc4c67bce8ffdb2ceeaf117e63d01ce80e4
stvyn-namdy <webmetcreatives@gmail.com>
2024-02-12 00:57:38 -0700

    building an ecommerce website using nodejs

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const rootDir = require('./utils/path');
+
+const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(rootDir, 'views', '404.html');
+});
+
+app.listen(3001);
